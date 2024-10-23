fbb3c62789821ae4f7161a4657c45602f1de3a3a
vladimir <vlj2007@yandex.ru>
2024-10-23 23:39:01 +0700

    Created new code

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,10 +1,22 @@
 static void main(String[] args) {
-  def dir = new File('file.txt');
-  if(dir.isDirectory()){
-    dir.listFiles()
-  } else {
+  //Создаем переменную для исходного каталога
+  def dir = new File('/tmp/1')
+  //Создаем переменную для счетчика каталогов
+  def countFiles = 0
+  //Создаем переменную для счетчика файлов
+  def countDirectories = 0
 
+  if (dir.isDirectory()){
+    for (File item : dir.listFiles()) {
+      if (item.isDirectory()) {
+        //println "The directories name is " + item.getName()
+        countDirectories = countDirectories + 1
+      } else {
+        //println "The file name is " + item.getName()
+        countFiles = countFiles + 1
+      }
+    }
   }
-
-  println "Hello world!"
+  println("The directories " + countDirectories)
+  println("The files " + countFiles)
 }
