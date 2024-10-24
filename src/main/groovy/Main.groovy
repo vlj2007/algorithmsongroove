437ebe9d5af100a6c9333daa5c1221a96b2aa315
vladimir <vlj2007@yandex.ru>
2024-10-24 23:52:31 +0700

    I created method isDirectoryExists() and if for checked my directory

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,22 +1,40 @@
-static void main(String[] args) {
-  //Создаем переменную для исходного каталога
-  def dir = new File('/tmp/1')
-  //Создаем переменную для счетчика каталогов
-  def countFiles = 0
-  //Создаем переменную для счетчика файлов
-  def countDirectories = 0
+import java.nio.file.Paths;
+static void main(String[] args) throws IOException {
+    //Создаем переменную для исходного каталога
 
-  if (dir.isDirectory()){
-    for (File item : dir.listFiles()) {
-      if (item.isDirectory()) {
-        //println "The directories name is " + item.getName()
-        countDirectories = countDirectories + 1
-      } else {
-        //println "The file name is " + item.getName()
-        countFiles = countFiles + 1
-      }
+    String d = '/tmp/1'
+    def dir = new File(d)
+    println("Directory Exists = " +  isDirectoryExists(d));
+
+    if (!isDirectoryExists(d)) {
+        println(dir.createParentDirectories().mkdir())
+        println("Directory was created " + dir.getName())
     }
-  }
-  println("The directories " + countDirectories)
-  println("The files " + countFiles)
+
+    //Создаем переменную для счетчика каталогов
+    def countFiles = 0
+    //Создаем переменную для счетчика файлов
+    def countDirectories = 0
+    if (dir.isDirectory()) {
+        for (File item : dir.listFiles()) {
+            if (item.isDirectory()) {
+                //println "The directories name is " + item.getName()
+                countDirectories = countDirectories + 1
+            } else {
+                //println "The file name is " + item.getName()
+                countFiles = countFiles + 1
+            }
+        }
+    }
+    println("The directories " + countDirectories)
+    println("The files " + countFiles)
+}
+
+
+static boolean isDirectoryExists(String directoryPath){
+    if (!Paths.get(directoryPath).toFile().isDirectory())
+    {
+        return false;
+    }
+    return true;
 }
